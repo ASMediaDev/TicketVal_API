aeb30535e71021bc5d8b5090af8ca205eba5dd15
Alexander Seitz <asmediadev@gmail.com>
2017-01-28 16:43:17 +0100

    -added eventid in response

--- getAttendees.php
+++ getAttendees.php
@@ -12,7 +12,7 @@ require("db/dbh_attendize.php");
 $eventId = $_GET['eventId'];
 
 
-$sql = "SELECT order_id, ticket_id, first_name, last_name, private_reference_number  FROM attendees WHERE event_id = '$eventId'";
+$sql = "SELECT order_id, ticket_id, event_id, first_name, last_name, private_reference_number  FROM attendees WHERE event_id = '$eventId'";
 $result = mysqli_query($conn, $sql);
 
 $attendeesarray = array();
